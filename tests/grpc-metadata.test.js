2d953429e730df7350e70994b645dcca3143ec1c
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2020-03-06 13:29:49 +0000

    metadata test use mock function, and only one as the second was irrelevant

--- tests/grpc-metadata.test.js
+++ tests/grpc-metadata.test.js
@@ -3,20 +3,15 @@ const grpc = require('grpc')
 const simpleGrpcClient = require('./simple-grpc-client')
 const simpleGrpcServer = require('./simple-grpc-server')
 
-const echo = async call => {
+const echo = jest.fn().mockImplementation(async call => {
   // console.log(call.request)
   // console.log(call.metadata)
   // console.log(call.metadata.get('role'))
   return {event: 'echo-reply', version: '0.1', message: call.request.message}
-}
-const doSomething = call => {
-  console.log('doing something...', call)
-  return {}
-}
+})
 
 const rpcs = {
   echo: callbackify(echo),
-  doSomething: callbackify(doSomething),
 }
 const grpcServiceConfig = {
   port: 50102,
@@ -43,7 +38,7 @@ test('Server is started', () => {
   expect(testServer.started).toBe(true)
 })
 
-test('gRPC client-server communication', done => {
+test('gRPC basic client-server communication works', done => {
   client.echo({message: 'hi'}, (err, response) => {
     expect(err).toBe(null)
     expect(response.message).toBe('hi')
@@ -56,15 +51,20 @@ test('gRPC send metadata', done => {
   meta.add('role', 'user')
   console.log('meta', meta)
   client.echo({message: 'hi'}, meta, (err, response) => {
+    expect(err).toBe(null)
+    expect(response.message).toBe('hi')
+    expect(echo).toHaveBeenCalled()
+    // expect(echo).toHaveBeenCalledWith({}, {})
+    // expect(echo.mock.calls).toBe([])
     done()
   })
 })
 
-test.skip('gRPC CallCredentials', done => {
-  const meta = new grpc.Metadata()
-  meta.add('role', 'user')
-  const callCredentials = grpc.credentials.createFromMetadataGenerator(meta)
-  client.echo({message: 'hi, with metadata'}, callCredentials, (err, response) => {
-    done()
-  })
-})
+// test.skip('gRPC CallCredentials', done => {
+//   const meta = new grpc.Metadata()
+//   meta.add('role', 'user')
+//   const callCredentials = grpc.credentials.createFromMetadataGenerator(meta)
+//   client.echo({message: 'hi, with metadata'}, callCredentials, (err, response) => {
+//     done()
+//   })
+// })
