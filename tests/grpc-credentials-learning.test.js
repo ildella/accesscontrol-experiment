194ba3bd7dbbcc93db46c10d89a5b99d074c4fb5
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2020-03-10 11:30:23 +0000

    tryint to use the interceptor to apply the role-acl validation

--- tests/grpc-credentials-learning.test.js
+++ tests/grpc-credentials-learning.test.js
@@ -1,21 +1,21 @@
-// const {GoogleAuth} = require('google-auth-library')
+const {GoogleAuth} = require('google-auth-library')
 const grpc = require('grpc')
 
-// const {credentials} = grpc
+const {credentials} = grpc
 
-// test('credentials', () => {
-//   const callCredentials = credentials.createFromMetadataGenerator({role: 'user'})
-//   // expect(callCredentials).toEqual({})
-// })
+test('credentials', () => {
+  const callCredentials = credentials.createFromMetadataGenerator({role: 'user'})
+  // expect(callCredentials).toEqual({})
+})
 
-// test('call credentials from Google', done => {
-//   (new GoogleAuth()).getApplicationDefault((err, auth) => {
-//     const googleCallCreds = grpc.credentials.createFromGoogleCredential(auth)
-//     // const combinedCreds = grpc.credentials.combineChannelCredentials(ssl_creds, call_creds)
-//     // expect(googleCallCreds).toBe({})
-//     done()
-//   })
-// })
+test('call credentials from Google', done => {
+  (new GoogleAuth()).getApplicationDefault((err, auth) => {
+    const googleCallCreds = grpc.credentials.createFromGoogleCredential(auth)
+    // const combinedCreds = grpc.credentials.combineChannelCredentials(ssl_creds, call_creds)
+    // expect(googleCallCreds).toBe({})
+    done()
+  })
+})
 
 const grpcServiceConfig = {
   port: 50102,
@@ -27,7 +27,8 @@ const fs = require('fs')
 const cert = fs.createReadStream('cert.pem')
 const key = fs.createReadStream('key.pem')
 
-test('load server with Channel Credentials from SSL Certificate', () => {
+// enable: learn from grpc-node test-server-credentials.ts
+test.skip('load server with Channel Credentials from SSL Certificate', () => {
   const {host, port} = grpcServiceConfig
   const server = new grpc.Server()
   const address = `${host}:${port}`
--- tests/grpc-interceptors.test.js
+++ tests/grpc-interceptors.test.js
@@ -1,4 +1,10 @@
+const {callbackify} = require('util')
 const grpc = require('grpc')
+const protoLoader = require('@grpc/proto-loader')
+
+const simpleGrpcServer = require('./simple-grpc-server')
+const simpleGrpcClient = require('./simple-grpc-client')
+
 const interceptor = new grpc.InterceptingCall({
   next_call: param => {
     console.log(param)
@@ -16,8 +22,43 @@ const interceptor = new grpc.InterceptingCall({
   }
 })
 
-// learn from --> https://github.com/echo-health/node-grpc-interceptors
+const echo = jest.fn().mockImplementation(async call =>
+  ({event: 'echo-reply', version: '0.1', message: call.request.message})
+)
+const rpcs = {
+  echo: callbackify(echo),
+}
+const grpcServiceConfig = {
+  port: 50102,
+  protoPath: `${__dirname}/something.proto`,
+  service: 'SomethingService',
+}
 
-test('intercept call', () => {
-  
+// const {host, port, protoPath} = grpcServiceConfig
+// const protoOptions = require('./default-proto-options')
+const {server, protoDescriptor} = simpleGrpcServer(grpcServiceConfig, rpcs)
+// const packageDefinition = protoLoader.loadSync(protoPath, protoOptions)
+// const protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
+// const server = new grpc.Server()
+// const address = `${host}:${port}`
+// const credentials = grpc.ServerCredentials.createInsecure()
+// server.bind(address, credentials)
+server.addService(protoDescriptor.proto['SomethingService'].service, rpcs)
+server.start()
+const client = simpleGrpcClient(grpcServiceConfig)
+
+afterAll(done => {
+  server.tryShutdown(() => done())
+})
+
+test('Server is started', () => {
+  expect(server.started).toBe(true)
+})
+
+test('gRPC basic client-server communication works', done => {
+  client.echo({message: 'hi'}, (err, response) => {
+    expect(err).toBe(null)
+    expect(response.message).toBe('hi')
+    done()
+  })
 })
